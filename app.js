c97da136c40599d1b65c4427c9f4941b23cd19c8
Teballo Candice Mokoena <145970563+CandiceMokoena@users.noreply.github.com>
2024-08-19 12:54:14 +0200

    added an alert on js

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-function Submit() {
-  alert("Thank you, we'll keep in touch");
-}
-
-let SubmitButton = document.querySelector("button");
-
-SubmitButton.addEventListener("click, Submit");
+alert{"hello"}
